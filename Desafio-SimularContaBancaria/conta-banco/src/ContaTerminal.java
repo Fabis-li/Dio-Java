70c15f9892f054f63e675a4370063b11113848a2
Fabis-li <fabiano_slima@hotmail.com>
2023-08-17 11:10:17 -0300

    commit inicial conta bancaria

---
+++ Desafio-SimularContaBancaria/conta-banco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+
+        //TODO:Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para os usuarios
+
+        //obter pela scanner os valores digitados no terminal
+
+        //exibir a mensagem conta criada
+       
+    }
+    
+}
